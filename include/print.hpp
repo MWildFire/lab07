9413537bc2b3ed1cf7c3a51331fff59f8ced058d
} <@gmail.com>
2018-05-16 21:40:42 +0300

    added documentation

--- include/print.hpp
+++
@@ -1,6 +0,0 @@
-#include <string>
-#include <fstream>
-#include <iostream> ///< Includig directories
-
-void print(const std::string& text, std::ostream& out = std::cout);///< Method Print
-void print(const std::string& text, std::ofstream& out);
